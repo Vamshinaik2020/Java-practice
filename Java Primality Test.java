9ee8b09f61009f224f291ba0ed6bfe22052b0e3a
Vamshi Naik <108263271+Vamshinaik2020@users.noreply.github.com>
2022-07-14 09:56:46 +0530

    Add files via upload

---
+++ Java Primality Test.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+
+        /* Used isProbablePrime(int certainty) as asked in the question and set certainty to 1  */
+
+        BigInteger number = new BigInteger(n);
+        if (number.isProbablePrime(1)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+
+}
